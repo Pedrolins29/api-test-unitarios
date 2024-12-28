61ce7b1c3325869c5d70d246b97c4820c95a3a62
PEDRO LINS <pedro.hcl29@gmail.com>
2024-12-28 13:14:11 -0300

    primeiro commit da aula de testes unitarios

---
+++ aula-de-testes-unitarios/src/main/java/br/com/api/aula/test/AulaDeTestesUnitariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.aula.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaDeTestesUnitariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaDeTestesUnitariosApplication.class, args);
+	}
+
+}
